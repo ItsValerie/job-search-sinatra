c5c8b7abdd57988aa8143a7929dd5599815bbeb9
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-05 21:56:46 +0200

    Styling edits

--- app.rb
+++ app.rb
@@ -11,8 +11,6 @@ require_relative 'app/models/job_opening'
 #DB setup
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
 
-# adding namespace?
-
 # ENDPOINTS
 
 # root
@@ -20,34 +18,36 @@ get '/' do
   "Welcome!"
 end
 
-# index - GET request to DB
+# index - GET request to DB (returns JSON string of last job query saved)
 get '/jobs' do
-  JobQuery.each do |query|
-    parsed_query = JSON.parse(query.results)
-    jobs = parsed_query['rss']['channel']['item']
-    unless jobs.nil? do
-      jobs.each do |job|
-        position = job['title']
-        company = job['author']['name']
-        skills = job['category']
-        website = job['link']
-        summary = job['description']
-        "#{position.to_json}"
-        saved_job_opening = build_job_opening(position, company, skills, website, summary)
-        # p "#{saved_job_opening.company.to_json}#{saved_job_opening.position.to_json}" unless saved_job_opening.nil?# testing purposes only
-      end
-    end
-  end
-  # convert to json again?
-  # print result in browser
+  last_job_query = JobQuery.last.results
+  ERB.new("#{last_job_query}").result
 end
 
-#GET request to Stackoverflow API
+# index - GET request to DB (not working yet)
+# get '/jobs' do
+#   JobQuery.each do |query|
+#     parsed_query = JSON.parse(query.results)
+#     job_openings = parsed_query['rss']['channel']['item']
+#     unless job_openings.nil? do
+#       job_openings.each do |job|
+#         position = job['title']
+#         company = job['author']['name']
+#         skills = job['category']
+#         website = job['link']
+#         summary = job['description']
+#         build_job_opening(position, company, skills, website, summary)
+#       end
+#     end
+#   end
+# end
+
+# GET request to Stackoverflow API
 get '/jobs/:keyword' do
   keyword = params['keyword']
-  handler = RequestHandler.new(keyword)
-  results_xml = handler.get_api_response(keyword)
-  json_string = Hash.from_xml(results_xml.to_s).to_json
+  handler = RequestHandler.new
+  xml_results = handler.get_api_response(keyword)
+  json_string = Hash.from_xml(xml_results.to_s).to_json
   parsed_query = JSON.parse(json_string)
   job_query = JobQuery.create!(results: json_string) unless parsed_query['rss']['channel']['totalResults'].to_i.zero?
   job_query.nil? ? "No results for this request" : "#{json_string}"
@@ -56,5 +56,5 @@ end
 private
 
 def build_job_opening(position, company, skills, website, summary)
-  JobOpening.create!(position: position, company: company, skills: skills, website: website, summary: summary)#.to_json
+  JobOpening.create!(position: position, company: company, skills: skills, website: website, summary: summary)
 end
--- app/models/request_handler.rb
+++ app/models/request_handler.rb
@@ -3,11 +3,6 @@ require 'nokogiri'
 require_relative 'job_query.rb'
 
 class RequestHandler
-
-  def initialize(keyword)
-    @keyword = keyword
-  end
-
   def get_api_response(keyword)
     url = "http://stackoverflow.com/jobs/feed?q=#{keyword}"
     xml = Nokogiri::XML(open(url)) do |config|
